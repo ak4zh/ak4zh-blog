5c3529a38fc195377a8766fabcc77aaef35138ad
ak4zh <agwl.akash@gmail.com>
2024-11-21 23:27:48 +0530

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,15 +12,6 @@
 
 <div class="container px-6 py-16 mx-auto text-center">
   <div class="max-w-lg mx-auto">
-    <h1 class="mb-3 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
-      Akash
-      <span
-        class="relative ml-2 inline-block before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-gradient-to-r from-blue-500 to-teal-500 dark:from-violet-500 dark:to-pink-500"
-        ><span class="relative skew-y-3 text-white">@ak4zh</span></span
-      >
-      Agarwal
-    </h1>
-
     <div class="mt-8 space-y-2">
       <p class="mt-6 text-gray-500 dark:text-gray-300">
         <span class="mx-2">A self-taught full stack developer</span>
